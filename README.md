72ac190e358db0c39ad80b52e767e70c48ead154
Rainer Helmes <rh@mifomm.eu>
2020-09-23 15:48:38 +0000

    admission validating service: mifomm.validation.svc

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kubernetes-controllers-and-operaters
+
+Learning about webhooks as well as about validating and mutating admission controllers
--- keygen.sh
+++ keygen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 KEYDIR="certs"
+mkdir -p $KEYDIR
 cd $KEYDIR
 
 # CA cert
@@ -8,8 +9,11 @@ openssl req -nodes -new -x509 -keyout ca.key -out ca.crt -subj "/CN=MiFoMM CA"
 # private key
 openssl genrsa -out mifomm.key 4096
 # Generate and sign the key
-openssl req -new -key mifomm.key -subj "/CN=admission-control-service.default.svc." \
+openssl req -new -key mifomm.key -subj "/CN=mifomm.validation.svc." \
     | openssl x509 -req -CA ca.crt -CAkey ca.key -CAcreateserial -out mifomm.crt 
 # Create .pem versions
 cp mifomm.crt mifommcrt.pem \
     | cp mifomm.key mifommkey.pem
+
+# Generate a single line base64 encoded certificate
+cat ca.crt | base64 | tr -d '\n' > cabase64.crt
